65a40c968c595919026982922c36a7586aa292ab
Qi Zhang <singularitti@outlook.com>
2019-04-12 00:31:22 -0400

    Finish Doolittle LU algorithm
    Fix some bugs

--- src/LUFactorization/SimpleLU.jl
+++ src/LUFactorization/SimpleLU.jl
@@ -22,7 +22,7 @@ function _row_vector(n::Int, k::Int)
 end
 
 function _elimination_factors(n::Int, k::Int, A::AbstractMatrix)
-    v = zeros(eltype(A), n)
+    v = zeros(promote_type(eltype(A), Float64), n)
     for j in (k + 1):n
         v[j] = A[j, k] / A[k, k]  # elimination factor
     end
@@ -38,8 +38,8 @@ function doolittle_lu(A::AbstractMatrix; include_l::Bool = true, include_e::Bool
     intermediate_elementary_elimination_matrices = []
     for k in 1:(n - 1)
         Ek = _general_elementary_elimination_matrix(_elimination_factors(n, k, A), _row_vector(n, k))
-        push!(intermediate_elementary_elimination_matrices, Ek)
-        A *= Ek
+        pushfirst!(intermediate_elementary_elimination_matrices, Ek)
+        A = Ek * A
     end
     # Now `A` is the U matrix.
     E = prod(*, intermediate_elementary_elimination_matrices)
