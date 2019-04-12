2a83f148315490337ca48391996173b3c2f32c79
Qi Zhang <singularitti@outlook.com>
2019-04-12 01:17:54 -0400

    Rename functions, deprecate `_`

--- src/LUFactorization/Doolittle.jl
+++ src/LUFactorization/Doolittle.jl
@@ -15,19 +15,19 @@ using LinearAlgebra: I, kron
 
 export doolittle_lu
 
-function _row_vector(n::Int, k::Int)
+function row_vector(n::Int, k::Int)
     v = zeros(n)
     v[k] = one(eltype(v))
     return v
 end
 
 """
-    _elimination_factors(n::Int, k::Int, A::AbstractMatrix)
+    elimination_factors(n::Int, k::Int, A::AbstractMatrix)
 
 Generate a vector of factors to eliminate the $k$th column.
-Combine this function with `_row_vector` to to generate an general elementary elimination matrix.
+Combine this function with `row_vector` to to generate an general elementary elimination matrix.
 """
-function _elimination_factors(n::Int, k::Int, A::AbstractMatrix)
+function elimination_factors(n::Int, k::Int, A::AbstractMatrix)
     v = zeros(promote_type(eltype(A), Float64), n)
     for j in (k + 1):n
         v[j] = A[j, k] / A[k, k]  # elimination factor
@@ -36,11 +36,11 @@ function _elimination_factors(n::Int, k::Int, A::AbstractMatrix)
 end
 
 """
-    _general_elementary_elimination_matrix(lk::AbstractVector, ek::AbstractVector)
+    general_elementary_elimination_matrix(lk::AbstractVector, ek::AbstractVector)
 
 Elimination matrix $E_k$ do row operations to eliminate the $k$th column.
 """
-_general_elementary_elimination_matrix(lk::AbstractVector, ek::AbstractVector) = I - kron(lk, transpose(ek))  # `I` is the identity matrix.
+general_elementary_elimination_matrix(lk::AbstractVector, ek::AbstractVector) = I - kron(lk, transpose(ek))  # `I` is the identity matrix.
 
 """
     doolittle_lu(A::AbstractMatrix; include_l::Bool=true, include_e::Bool=false)
@@ -53,7 +53,7 @@ function doolittle_lu(A::AbstractMatrix; include_l::Bool = true, include_e::Bool
 
     intermediate_elementary_elimination_matrices = []
     for k in 1:(n - 1)
-        Ek = _general_elementary_elimination_matrix(_elimination_factors(n, k, A), _row_vector(n, k))
+        Ek = general_elementary_elimination_matrix(elimination_factors(n, k, A), row_vector(n, k))
         pushfirst!(intermediate_elementary_elimination_matrices, Ek)  # Keep this order!
         A = Ek * A
     end
