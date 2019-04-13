4035a12d43ab543605a2602d3169f4fbd791fb8d
Qi Zhang <singularitti@outlook.com>
2019-04-12 21:28:42 -0400

    fix: Fix bugs in `substitution`
    Avoid `sum` over an empty array, and `BoundsError`

--- src/LUFactorization/Substitution.jl
+++ src/LUFactorization/Substitution.jl
@@ -13,6 +13,8 @@ module Substitution
 
 using LinearAlgebra: rank, UpperTriangular, LowerTriangular
 
+using APMAE4001.Matrix: issquare
+
 export Forward,
     Backward,
     substitution
@@ -23,20 +25,20 @@ struct Backward end
 function substitution(::Backward, A::AbstractMatrix, b::AbstractVector)
     issquare(A) || throw(DimensionMismatch("The matrix `A` is not a square matrix!"))
     m = size(A, 1)
-    r = rank(A)
-    solutions = [b[m] / A[m, m]]
-    for i in m:1
-        pushfirst!((b[i] - sum(A[i, j] * solutions[j] for j in (i + 1):m)) / A[i, i])
+    solutions = zeros(m)
+    solutions[m] = b[m] / A[m, m]
+    for i in (m - 1):-1:1
+        solutions[i] = (b[i] - sum(A[i, j] * solutions[j] for j in (i + 1):m)) / A[i, i]
     end
     solutions
 end
 function substitution(::Forward, A::AbstractMatrix, b::AbstractVector)
     issquare(A) || throw(DimensionMismatch("The matrix `A` is not a square matrix!"))
     m = size(A, 1)
-    r = rank(A)
-    solutions = [b[1] / A[1, 1]]
+    solutions = zeros(m)
+    solutions[1] = b[1] / A[1, 1]
     for i in 2:m
-        push!((b[i] - sum(A[i, j] * solutions[j] for j in 1:(i - 1))) / A[i, i])
+        solutions[i] = (b[i] - sum(A[i, j] * solutions[j] for j in 1:(i - 1))) / A[i, i]
     end
     solutions
 end
