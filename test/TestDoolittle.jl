3fdcabaebaa3ebf9e7884090372a55db36299ca5
Qi Zhang <singularitti@outlook.com>
2019-04-12 21:32:07 -0400

    Add a test for `polysolve`

--- test/TestDoolittle.jl
+++ test/TestDoolittle.jl
@@ -41,6 +41,7 @@ end
     A = [1  1   0
          2  5   1
          1  4  -1]
+     b = [1, 1, 2]
     L, U = doolittle_lu(A)
     @test L == [1.0  0.0  0.0
                 2.0  1.0  0.0
@@ -48,6 +49,8 @@ end
     @test U == [1.0  1.0   0.0
                 0.0  3.0   1.0
                 0.0  0.0  -2.0]
+     solutions = polysolve(A, b)
+     @test solutions == A \ b
 end
 
 end
