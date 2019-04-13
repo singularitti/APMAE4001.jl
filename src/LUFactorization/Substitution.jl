d7d6d9b520f7642deb7054f29119c536639558c7
Qi Zhang <singularitti@outlook.com>
2019-04-12 21:26:44 -0400

    docs: Rename symbol in doc

--- src/LUFactorization/Substitution.jl
+++ src/LUFactorization/Substitution.jl
@@ -21,22 +21,24 @@ struct Forward end
 struct Backward end
 
 function substitution(::Backward, A::AbstractMatrix, b::AbstractVector)
-    issquare(A) || throw(DimensionMismatch("The matrix `a` is not a square matrix!"))
+    issquare(A) || throw(DimensionMismatch("The matrix `A` is not a square matrix!"))
     m = size(A, 1)
     r = rank(A)
     solutions = [b[m] / A[m, m]]
     for i in m:1
         pushfirst!((b[i] - sum(A[i, j] * solutions[j] for j in (i + 1):m)) / A[i, i])
     end
+    solutions
 end
 function substitution(::Forward, A::AbstractMatrix, b::AbstractVector)
-    issquare(A) || throw(DimensionMismatch("The matrix `a` is not a square matrix!"))
+    issquare(A) || throw(DimensionMismatch("The matrix `A` is not a square matrix!"))
     m = size(A, 1)
     r = rank(A)
     solutions = [b[1] / A[1, 1]]
     for i in 2:m
         push!((b[i] - sum(A[i, j] * solutions[j] for j in 1:(i - 1))) / A[i, i])
     end
+    solutions
 end
 substitution(A::UpperTriangular, b::AbstractVector) = substitution(Backward(), A.data, b)
 substitution(A::LowerTriangular, b::AbstractVector) = substitution(Forward(), A.data, b)
