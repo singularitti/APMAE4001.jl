c2d0355b42b1a7d4c15dc6c55911ec448f44a137
Qi Zhang <singularitti@outlook.com>
2019-04-27 14:46:49 -0400

    Add `isdiagonallydominant` to Matrix.jl

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -11,11 +11,22 @@ julia>
 """
 module Matrix
 
-export issquare
+export issquare,
+    isdiagonallydominant
 
 function issquare(A::AbstractMatrix)::Bool
     m, n = size(A)
     m == n ? true : false
+end
+
+function isdiagonallydominant(A::AbstractMatrix)::Bool
+    issquare(A) || error("Unsupported!")
+    n = size(A, 1)
+    B = abs.(A)
+    for i in 1:n
+        B[i, i] > mapreduce(j->!isequal(j, i), +, 1:n) || return false
+    end
+    return true
 end
 
 end
