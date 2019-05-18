44a1ad207d39e4e812eabbcbeac927210b23bc8c
Qi Zhang <singularitti@outlook.com>
2019-05-17 23:31:46 -0400

    Allow `matrix_form` in `gram_schmidt` method

--- src/Projections/GramSchmidt.jl
+++ src/Projections/GramSchmidt.jl
@@ -13,6 +13,8 @@ module GramSchmidt
 
 using LinearAlgebra: norm, normalize
 
+using APMAE4001.Tools: deepflatten
+
 export project_onto_vector,
     gram_schmidt
 
@@ -20,12 +22,13 @@ function project_onto_vector(a::Vector, b::Vector)
     a * a' * b / norm(a)^2
 end  # function project_onto_vector
 
-function gram_schmidt(A; normalized::Bool = true)
+function gram_schmidt(A; normalized::Bool = true, matrix_form::Bool = true)
     raw = [A[1]]
     for i in 2:length(A)
         push!(raw, A[i] - sum(project_onto_vector(A[j], A[i]) for j in 1:(i - 1)))
     end
-    normalized ? map(normalize, raw) : raw
+    x = (normalized ? map(normalize, raw) : raw)
+    matrix_form ? deepflatten(x) : x
 end  # function gram_schmidt
 
 end
